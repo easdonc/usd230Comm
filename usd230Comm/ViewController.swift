3d4670af04fa04843a7332939c9e99bf984b626c
easdonc <easdonc@usd230.org>
2015-12-09 14:33:58 -0600

    Initial Commit

---
+++ usd230Comm/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  usd230Comm
+//
+//  Created by Colton Easdon on 12/9/15.
+//  Copyright © 2015 USD230IT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
